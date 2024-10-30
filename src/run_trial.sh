f1a1d23c44d5593d8890593c85b36465018d9a8d
Magraz <agrazvallejo@live.com>
2024-10-30 16:37:48 -0700

    updated yamls

--- src/run_trial.sh
+++ src/run_trial.sh
@@ -1,3 +1,3 @@
 TRIAL_ID=${1}
-python3 /home/magraz/vmas_rovers/src/run_ccea.py --experiment_type sanity  --poi_type static --model mlp --trial_id ${TRIAL_ID}
+python3 /home/magraz/vmas_rovers/src/run_ccea.py --batch mlp_fc  --name d_mlp --trial_id ${TRIAL_ID}
 echo "Finished Trial #${TRIAL_ID}"
